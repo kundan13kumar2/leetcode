d39a715ccbe6b877dfde2c8573cae90819795997
kundan.kumar <kundan.kumar@ringcentral.com>
2025-01-26 13:21:18 +0530

    AdaptorPattern

---
+++ src/main/java/LLD/weightMachineAdaptor/babyWeightmachine/WeightMachineImpl.java
@@ -0,0 +1,12 @@
+package LLD.weightMachineAdaptor.babyWeightmachine;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class WeightMachineImpl implements WeightMachine {
+
+    @Override
+    public double getWeightInPound() {
+        return 28;
+    }
+}
