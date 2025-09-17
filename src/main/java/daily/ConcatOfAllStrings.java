1c39e3cf343d103be72e525883e6c381cc334e20
Kundan <kundan94.mnnit@gmail.com>
2025-09-17 18:46:18 +0530

    Daily Code

---
+++ src/main/java/daily/ConcatOfAllStrings.java
@@ -0,0 +1,24 @@
+package daily;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class ConcatOfAllStrings {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        HashSet<String> set = new HashSet<>();
+        int wordLen = words[0].length();
+        for (String word : words) {
+            set.add(word);
+        }
+
+
+
+
+        return new ArrayList<>();
+    }
+}
